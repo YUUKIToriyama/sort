0f294dfb7c85261f1daf728dbed974519fa8c98c
YUUKIToriyama <github@toriyama.dev>
2022-04-14 15:16:41 +0900

    シェーカーソートを実装

--- src/lib.rs
+++ src/lib.rs
@@ -14,19 +14,60 @@ pub mod sort {
         }
         return arr;
     }
+
+    /// シェーカーソート
+    pub fn shaker_sort(mut arr: Vec<i32>) -> Vec<i32> {
+        let l = arr.len() - 1;
+        for i in 0..l {
+            for j in i..l {
+                if i % 2 == 0 {
+                    let a = arr[j];
+                    let b = arr[j + 1];
+                    if a > b {
+                        arr[j + 1] = a;
+                        arr[j] = b;
+                    }
+                } else {
+                    let c = arr[l - j - 1];
+                    let d = arr[l - j];
+                    if c > d {
+                        arr[l - j] = c;
+                        arr[l - j - 1] = d;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn test_cases() -> Vec<(Vec<i32>, Vec<i32>)> {
+        return vec![
+            (vec![1, 2, 3], vec![1, 2, 3]),
+            (vec![9, 5, 1], vec![1, 5, 9]),
+            (vec![99, 33, 55, 11], vec![11, 33, 55, 99]),
+            (
+                vec![1, 9, 2, 8, 3, 7, 4, 6, 5],
+                vec![1, 2, 3, 4, 5, 6, 7, 8, 9],
+            ),
+        ];
+    }
+
     #[test]
     fn test_bubble_sort() {
-        assert_eq!(sort::bubble_sort(vec![1, 2, 3]), vec![1, 2, 3]);
-        assert_eq!(sort::bubble_sort(vec![9, 5, 1]), vec![1, 5, 9]);
-        assert_eq!(sort::bubble_sort(vec![99, 33, 55]), vec![33, 55, 99]);
-        assert_eq!(
-            sort::bubble_sort(vec![1, 9, 2, 8, 3, 7, 4, 6, 5]),
-            vec![1, 2, 3, 4, 5, 6, 7, 8, 9]
-        );
+        for test_case in test_cases() {
+            assert_eq!(sort::bubble_sort(test_case.0), test_case.1);
+        }
+    }
+
+    #[test]
+    fn test_shaker_sort() {
+        for test_case in test_cases() {
+            assert_eq!(sort::shaker_sort(test_case.0), test_case.1);
+        }
     }
 }
