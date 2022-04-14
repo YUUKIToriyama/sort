23e0a3e9e588b288a4d7b6d0a0e657561e450120
YUUKIToriyama <github@toriyama.dev>
2022-04-14 15:38:49 +0900

    コムソートを実装

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,33 @@ pub mod sort {
         }
         return arr;
     }
+
+    /// コムソート
+    pub fn comb_sort(mut arr: Vec<i32>) -> Vec<i32> {
+        let mut h = arr.len() * 10 / 13;
+        loop {
+            let mut swap_count = 0;
+            let mut i = 0;
+            while i + h < arr.len() {
+                let a = arr[i];
+                let b = arr[i + h];
+                if a > b {
+                    arr[i + h] = a;
+                    arr[i] = b;
+                    swap_count = swap_count + 1;
+                }
+                i = i + 1;
+            }
+            if h == 1 {
+                if swap_count == 0 {
+                    break;
+                }
+            } else {
+                h = h * 10 / 13;
+            }
+        }
+        return arr;
+    }
 }
 
 #[cfg(test)]
@@ -68,6 +95,13 @@ mod tests {
     fn test_shaker_sort() {
         for test_case in test_cases() {
             assert_eq!(sort::shaker_sort(test_case.0), test_case.1);
+        }
+    }
+
+    #[test]
+    fn test_comb_sort() {
+        for test_case in test_cases() {
+            assert_eq!(sort::comb_sort(test_case.0), test_case.1);
         }
     }
 }
